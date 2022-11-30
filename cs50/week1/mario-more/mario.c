9176279c82c7aea895eaa8e407d317744ccb650d
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 03:19:14 +0000

    Wed, Nov 30, 2022, 3:19 AM +00:00

--- cs50/week1/mario-more/mario.c
+++ cs50/week1/mario-more/mario.c
@@ -18,7 +18,7 @@ int main(void)
     for(i=0; i < n; i++)
     {
         //For each column
-        for(j=0; j < i; j++)
+        for(j=0; j <= i; j++)
         {
             printf("#");
         }
