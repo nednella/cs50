2816c3f8fd0bb37b589ff6e288efe254bce877e9
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 20:05:33 +0000

    Wed, Nov 30, 2022, 8:05 PM +00:00

--- cs50/week1/cash/cash.c
+++ cs50/week1/cash/cash.c
@@ -44,15 +44,6 @@ int get_cents(void) {
     return cents;
 }
 
-int get_cents(void)
-{
-    int cents = get_int("Change owed: ");
-    return cents;
-}
-
-
-
-
 int calculate_quarters(int cents) {
     int quarters = cents/25;
     return quarters;
