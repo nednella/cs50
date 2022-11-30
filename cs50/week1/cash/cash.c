eeffc238cea9380ab4bace42f9b20b7ea7874fe2
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 18:55:02 +0000

    Wed, Nov 30, 2022, 6:55 PM +00:00

--- cs50/week1/cash/cash.c
+++ cs50/week1/cash/cash.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_cents(void);
+int get_cents(int cents);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
@@ -10,7 +10,7 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents();
+    int cents = get_cents("Change owed: ");
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
@@ -35,10 +35,9 @@ int main(void)
     printf("%i\n", coins);
 }
 
-int get_cents(void)
+int get_cents(int cents)
 {
     // TODO
-    int cents = get_cents("?");
     return 0;
 }
 
