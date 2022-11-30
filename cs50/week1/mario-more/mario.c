501da3a2fbbd1acaecae7e21c3d87a419ecf4e57
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 02:36:45 +0000

    Wed, Nov 30, 2022, 2:36 AM +00:00

--- cs50/week1/mario-more/mario.c
+++ cs50/week1/mario-more/mario.c
@@ -4,6 +4,8 @@
 int main(void)
 {
     int n;
+    int i;
+    int j;
 
     do
     {
@@ -12,21 +14,18 @@ int main(void)
     //Ensure n is between 1 and 8
     while ((n <= 0) || (n > 8));
 
-    int i;
+
 
     //For each row
     for(i=0; i < n; i++)
     {
         //For each column
-        int j;
-
         for(j=0; j < n; j++)
         {
-            printf("#");
+            printf("##");
         }
 
         // Move to the next row
         printf("\n");
-
     }
 }
