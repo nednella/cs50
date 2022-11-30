56695e7f247296fb5b0f879d68bf2d71e0ff0a02
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 18:30:30 +0000

    Wed, Nov 30, 2022, 6:30 PM +00:00

--- cs50/week1/mario-more/mario.c
+++ cs50/week1/mario-more/mario.c
@@ -1,7 +1,47 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n, i, j, k, l;
+
+    do
+    {
+        n = get_int("Height: ");
+    }
+    //Ensure n is between 1 and 8
+    while ((n <= 0) || (n > 8));
+
+
+
+
+    for (i = 0; i < n; i++)
+    {
+        //Code to produce right-aligned triangle
+        for (j = (i+1); j < n; j++)
+        {
+            printf(" ");
+        }
+
+        for (k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        //Space between triangles
+        printf("  ");
+
+        //Code to produce left-aligned triangle
+        for (l = 0; l <= i; l++)
+        {
+            printf("#");
+        }
+
+
+        printf("\n");
+    }
+
+
+
+
 }
