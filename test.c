98f0f91906123ffd6969fa50dc3a3c91a9d4093e
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 21:57:32 +0000

    Wed, Nov 30, 2022, 9:57 PM +00:00

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //Prompt user for input, and check if card number is valid (length)
+    long cc = get_long("Card Number: ");
+
+    //Is the card number a valid length (13-16 digits)?
+    if (cc < 999999999999 || cc > 9999999999999999) {
+        //The card number is invalid
+        printf("INVALID\n");
+    }
+    else {
+
+
+        int b = (cc % 10000000000000000  / 1000000000000000);
+        printf("%i is the result", b);
+    }
+}
