bb88295f448ad4d59993ce9848623d3284e26c3e
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 01:05:37 +0000

    Wed, Nov 30, 2022, 1:05 AM +00:00

---
+++ Junk/anthony_is_gay.c
@@ -0,0 +1,8 @@
+#include<cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("I promise I promise I'm not trying to be smart here, but technically- \n");
+    printf("No, no technically!\n");
+}
--- Junk/homo.c
+++
@@ -1,8 +0,0 @@
-#include<cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    string answer = get_string("Is Krep gay? ");
-    printf("%s? No way, he's definitely gay\n", answer);
-}
