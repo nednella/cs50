beb4b9a48c822a36a7d22129599c73fa7d7f682b
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 22:57:21 +0000

    Wed, Nov 30, 2022, 10:57 PM +00:00

--- cs50/week1/credit/credit.c
+++ cs50/week1/credit/credit.c
@@ -14,8 +14,6 @@ int main(void)
     else {
         // The card number is valid
 
-        // Luhn Algorithm
-
         // Obtain every individual card number, back -> front
         int n16 = (cc % 10000000000000000) / 1000000000000000;  // 16th number from the back
         int n15 = (cc % 1000000000000000)  / 100000000000000;   // 15th number from the back
@@ -34,73 +32,28 @@ int main(void)
         int n2  = (cc % 100)               / 10;                // 2nd number from the back
         int n1  = (cc % 10)                / 1;                 // 1st number from the back
 
+        // Luhn Algorithm
+        // Multiply every 2nd number from the back by 2
 
-
-        //create rray of 9 elements
-
-
-
-
-
-        int n[9];
-
-        for (int i = 0; i < 10; i++) {
-            n[i] = 0; // Fill array with 0
-        }
-
-        //obtain every 2nd number starting from the back
-            //set element equal to last X numbers
-            //reduce element to just the 1st digit of the last X numbers
-        n[1] = (cc % 100)               / 10;               //obtains 2nd last number
-        n[2] = (cc % 10000)             / 1000;             //obtains 4th last number
-        n[3] = (cc % 1000000)           / 100000;           //obtains 6th last number
-        n[4] = (cc % 100000000)         / 10000000;         //obtains 8th last number
-        n[5] = (cc % 10000000000)       / 1000000000;       //obtains 10th last number
-        n[6] = (cc % 1000000000000)     / 100000000000;     //obtains 12th last number
-        n[7] = (cc % 100000000000000)   / 10000000000000;   //obtains 14th last number
-        n[8] = (cc % 10000000000000000) / 1000000000000000; //obtains 16th last number
-
-        //Multiply every 2nd number by 2
-        int d[9];
-
-        for (int d = 0; d < 10; d++) {
-            d[i] = 0; //Fill aray with 0
-        }
-
-        d[1] = (n[1] * 2);
-        d[2] = (n[2] * 2);
-        d[3] = (n[3] * 2);
-        d[4] = (n[4] * 2);
-        d[5] = (n[5] * 2);
-        d[6] = (n[6] * 2);
-        d[7] = (n[7] * 2);
-        d[8] = (n[8] * 2);
-
-        //
-
-
-        m[1] = first_digit(d[1])
-
-
-        //printf("Success\n");
+        int m1 = n2 * 2;
+        int m2 = n4 * 2;
+        int m3 = n6 * 2;
+        int m4 = n8 * 2;
+        int m5 = n10 * 2;
+        int m6 = n12 * 2;
+        int m7 = n14 * 2;
+        int m8 = n16 * 2;
 
 
 
 
 
 
-    }
 
 
 
 
-    //checksum( )
 
-    //If invalid
-    //printf("INVALID\n");
-
-    //If valid
-    //Check for card length and 2 initial card digits
 
 
 
