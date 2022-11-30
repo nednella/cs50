2fc9281d3fe99fb38653af71df1d2b56d076d77c
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 21:05:46 +0000

    Wed, Nov 30, 2022, 9:05 PM +00:00

--- cs50/week1/credit/credit.c
+++ cs50/week1/credit/credit.c
@@ -4,19 +4,26 @@
 int main(void)
 {
     //Prompt user for input, and check if card number is valid (length)
-    long cn; //card number
+    long cn = get_long("Card Number: ");
+
 
     do {
-        cn = get_long("Card Number: ");
+
     }
     while (cn < 999999999999 || cn > 9999999999999999);
+
+
+
+
+
+
+    //Is the card number a valid length (13-16 digits)?
+    if (cn < 999999999999 || cn > 9999999999999999, printf("INVALID\n"), break)
 
     printf("Success\n");
 
 
 
-
-    //Perform CHECKSUM - is the card number valid?
     //checksum( )
 
     //If invalid
