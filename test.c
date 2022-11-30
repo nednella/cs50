46f354eac73e32d1d458838555632110c6e671c8
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 22:16:48 +0000

    Wed, Nov 30, 2022, 10:16 PM +00:00

--- test.c
+++ test.c
@@ -13,8 +13,23 @@ int main(void)
     }
     else {
 
+        long a = (cc % 100)               / 10;
+        long b = (cc % 10000)             / 1000;
+        long c = (cc % 1000000)           / 100000;
+        long d = (cc % 100000000)         / 10000000;
+        long e = (cc % 10000000000)       / 1000000000;
+        long f = (cc % 1000000000000)     / 100000000000;
+        long g = (cc % 100000000000000)   / 10000000000000;
+        long h = (cc % 10000000000000000) / 1000000000000000;
 
-        long b = (cc % 10000000000000000  / 10000000000000000);
+
+        printf("%li is the result\n", a);
         printf("%li is the result\n", b);
+        printf("%li is the result\n", c);
+        printf("%li is the result\n", d);
+        printf("%li is the result\n", e);
+        printf("%li is the result\n", f);
+        printf("%li is the result\n", g);
+        printf("%li is the result\n", h);
     }
 }
