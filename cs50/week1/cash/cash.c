f42357aacc126066820e2a4ca783012b2881ac4f
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 19:59:23 +0000

    Wed, Nov 30, 2022, 7:59 PM +00:00

--- cs50/week1/cash/cash.c
+++ cs50/week1/cash/cash.c
@@ -12,9 +12,9 @@ int main(void) {
     int cents;
 
     do {
-        int cents = get_cents();
+        cents = get_cents();
     }
-    while (cents < 0)
+    while (cents < 0);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
