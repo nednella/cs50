16299b4802d94b7c332617faa2d921bc90c65021
benallenden <119449811+benallenden@users.noreply.github.com>
2022-11-30 20:27:52 +0000

    Wed, Nov 30, 2022, 8:27 PM +00:00

--- cs50/week1/credit/credit.c
+++ cs50/week1/credit/credit.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long card = get_long("Card Number: ");
+
+
+    checksum( )
+
+
+
+
+
+    printf("Success\n");
 }
