467fdf6dc2c09479cbbdb74ddb9f12943d2f99fd
benallenden <119449811+benallenden@users.noreply.github.com>
2022-12-01 01:35:13 +0000

    Thu, Dec 1, 2022, 1:35 AM +00:00

--- cs50/week1/credit/credit.c
+++ cs50/week1/credit/credit.c
@@ -74,6 +74,8 @@ int main(void)
 
         int sum = sum_pt1 + sum_pt2;
 
+        printf("%i\n", sum);
+
         // (4) Validate that the last number of int_sum is a 0
         if (sum % 10 != 0) {
             // The card number itself is not valid as per the Luhn Algorithm
