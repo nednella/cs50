602f8a662a97ca01a8cbcf607deda6d102787a48
benallenden <119449811+benallenden@users.noreply.github.com>
2022-12-01 00:54:50 +0000

    Thu, Dec 1, 2022, 12:54 AM +00:00

--- test.c
+++ test.c
@@ -3,6 +3,16 @@
 
 int main(void) {
 
-    a
+    int n;
+    int count = 0;
+
+    n = get_int("Enter a Number: ");
+
+    while (n != 0) {
+        n = n / 10;
+        count ++;
+    }
+
+    printf("Number of digits: %i\n", count);
 
 }
