597756588d3c977aa46530581427faf4919612f7
benallenden <119449811+benallenden@users.noreply.github.com>
2022-12-01 01:43:43 +0000

    Thu, Dec 1, 2022, 1:43 AM +00:00

--- cs50/week1/credit/credit.c
+++ cs50/week1/credit/credit.c
@@ -121,18 +121,22 @@ int main(void)
             else if (count == 16) {
                 // And the first digit is 4
                 if (n16 == 4) {
+                    // The card is VISA
                     printf("VISA");
                 }
 
+                // If the first digit is not 4, but the first 2 digits are 51, 52, 53, 54 or 55
                 else if (first_2_16 == 51 || first_2_16 == 52 || first_2_16 == 53 || first_2_16 == 54 || first_2_16 == 55) {
                     printf("MASTERCARD");
                 }
 
+                // Else, card is invalid
                 else {
                     printf("INVALID");
                 }
             }
 
+            //Else, card is invalid
             else {
                 printf("INVALID");
             }
