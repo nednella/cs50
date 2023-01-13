6f4a4707c25658c823dc4f13ef5164788dd061d7
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-13 21:40:30 +0000

    Fri, Jan 13, 2023, 9:40 PM +00:00

--- cs50/week2/wordle/wordle.c
+++ cs50/week2/wordle/wordle.c
@@ -77,6 +77,12 @@ int main(int argc, string argv[])
     printf(GREEN"This is WORDLE50"RESET"\n");
     printf("You have %i tries to guess the %i-letter word I'm thinking of\n", guesses, wordsize);
 
+
+
+
+
+
+
     // main game loop, one iteration for each guess
     for (int i = 0; i < guesses; i++)
     {
