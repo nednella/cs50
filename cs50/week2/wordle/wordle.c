c7cd91e1c3e5062604f8551ffa01e08a65f0210e
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-13 21:51:24 +0000

    Fri, Jan 13, 2023, 9:51 PM +00:00

--- cs50/week2/wordle/wordle.c
+++ cs50/week2/wordle/wordle.c
@@ -37,18 +37,22 @@ int main(int argc, string argv[])
 		return 1;
 	}
 
-    printf("%s\n", argv[1]);
-
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
-    //if (argv[1] != 5 || argv[1] != 6 || argv[1] != 7 || argv[1] != 8) {
-        //printf("ERROR: wordsize must be either 5, 6, 7 or 8\n");
-        //return 1;
-    //}
+    strtol (argv[1]);
 
-    //else {
-        //wordsize = atoi(argv[1]);
-        //printf("%i\n", wordsize);
-    //}
+    if (argv[1] != 5 || argv[1] != 6 || argv[1] != 7 || argv[1] != 8) {
+        printf("ERROR: wordsize must be either 5, 6, 7 or 8\n");
+        return 1;
+    }
+
+    else {
+        wordsize = atoi(argv[1]);
+        printf("%i\n", wordsize);
+    }
+
+
+
+
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
