f67499e046ad149c2e6ce0b8d87b08e3829394bd
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-13 21:15:48 +0000

    Fri, Jan 13, 2023, 9:15 PM +00:00

--- test.c
+++ test.c
@@ -1,15 +1,14 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <ctype.h>
+	#include <cs50.h>
+	#include <stdio.h>
 
-int main(void) {
-
-char a = get_char("Character: ");
-
-if(isalpha(a) != 0)
-printf("true");
-
-else
-printf("false");
-
+	int main(int argc, string argv[])
+	{
+		if (argc ==2)
+		{
+			printf("Hello, %s\n", argv[1]);
+		}
+		else
+		{
+			printf("Hello, world\n");
+		}
 }
