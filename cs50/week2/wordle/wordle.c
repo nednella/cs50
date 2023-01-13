e42e66c9d5ccb9a9cf5dfa9f22fc062eda3075fc
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-13 21:56:47 +0000

    Fri, Jan 13, 2023, 9:56 PM +00:00

--- cs50/week2/wordle/wordle.c
+++ cs50/week2/wordle/wordle.c
@@ -38,9 +38,8 @@ int main(int argc, string argv[])
 	}
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
-    atoi (argv[1]);
 
-    if (argv[1] != 5 || argv[1] != 6 || argv[1] != 7 || argv[1] != 8) {
+    if (atoi (argv[1]) != 5 || atoi (argv[1]) != 6 || atoi (argv[1]) != 7 || atoi (argv[1]) != 8) {
         printf("ERROR: wordsize must be either 5, 6, 7 or 8\n");
         return 1;
     }
