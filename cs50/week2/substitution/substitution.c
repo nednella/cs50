5a42df5ad963b03f55350884cbf31f6b81745764
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-14 23:03:35 +0000

    Sat, Jan 14, 2023, 11:03 PM +00:00

--- cs50/week2/substitution/substitution.c
+++ cs50/week2/substitution/substitution.c
@@ -3,7 +3,26 @@
 
 int main(int argc, string argv[]) {
 
-    
+    // if the command-line argument is not equal to 2 elements
+    if (argc != 2) {
+        // print a guide for correct usage
+        printf("Usage: ./substitution [key]");
+        // and terminate program
+        return 1;
+    }
+
+    // store the entered key from the CLI
+    string key = atoi(argv[1]);
+
+    // store the character length of the key
+    int keylength = strlen(key);
+
+    if (keylength != 26 && ) {
+        printf("Key must contain 26 characters.");
+    }
+
+
+
 
 
 
