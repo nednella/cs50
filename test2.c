7730e500e608923f3c2df46b9cd05bbfbe83b156
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-14 20:57:47 +0000

    Sat, Jan 14, 2023, 8:57 PM +00:00

--- test2.c
+++ test2.c
@@ -1,10 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 
 
 
 int main(void) {
+
+    // guess word
+    string guess = "games";
+
+    // string length of guess word
+    int guesslength = strlen(guess);
+
+    // create array of a size equal to the length of the guess word
+    int status[guesslength];
+
+    // set all values inside the array equal to 0
+    for (int i = 0; i < guesslength; i++) {
+        status[i] = 0;
+        printf("%i", status[i]);
+    }
+    printf("\n");
+
+    
+
+
 
 
 
