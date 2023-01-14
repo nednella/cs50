ac20a682c3dce378c4300f9bfe798b2008d36194
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-14 23:16:47 +0000

    Sat, Jan 14, 2023, 11:16 PM +00:00

--- cs50/week2/substitution/substitution.c
+++ cs50/week2/substitution/substitution.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 int main(int argc, string argv[]) {
@@ -7,13 +9,13 @@ int main(int argc, string argv[]) {
     // if the command-line argument is not equal to 2 elements
     if (argc != 2) {
         // print a guide for correct usage
-        printf("Usage: ./substitution [key]");
+        printf("Usage: ./substitution [key]\n");
         // and terminate program
         return 1;
     }
 
     // store the entered key from the CLI
-    string key = atoi(argv[1]);
+    string key = argv[1];
 
     // check for an invalid key
 
@@ -27,23 +29,20 @@ int main(int argc, string argv[]) {
     // check the contents of the key (for non-alphabetic characters)
     for (int i = 0; i < keylength; i++) {
 
-        printf("%c\n",key[i])
+        printf("%c\n",key[i]);
 
         if (isalpha(key[i])) {
             printf("The key must contain alphabetic characters only.");
             return 1;
         }
-        
 
 
 
     }
     // check for strict key length of 26 characters
     //if (keylength != 26 && ) {
-        printf("Key must contain 26 characters.");
+        //printf("Key must contain 26 characters.");
     //}
-
-
 
 
 
