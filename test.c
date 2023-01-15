36ca66eb485317e2c9fc4dbe2d3ba6f817671e84
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-15 01:05:41 +0000

    Sun, Jan 15, 2023, 1:05 AM +00:00

--- test.c
+++ test.c
@@ -16,18 +16,18 @@ int main(void) {
     printf("Key before sorting: %s", key);
     printf("\n");
 
-    printf("First character: %c\n", key[0]);
-    printf("First character: %c\n", key[1]);
+    //printf("First character: %c\n", key[0]);
+    //printf("First character: %c\n", key[1]);
 
     // bubble sort key ascending order
-    //for (int i = 0; i < (keylength - 1); i++) {
-        //for (int j = (i + 1); j < keylength; j++) {
-            //if (key[i] > key[j]) {
-                //char ChTemp = key[i];
-                //key[i] = key[j];
-                //key[j] = ChTemp;
-            //}
-        //}
-    //}
-    printf("Key after sorting: %s", key);
+    for (int i = 0; i < (keylength - 1); i++) {
+        for (int j = (i + 1); j < keylength; j++) {
+            if (key[i] > key[j]) {
+                char ChTemp = key[i];
+                key[i] = key[j];
+                key[j] = ChTemp;
+            }
+        }
+    }
+    printf("Key after sorting: %s\n", key);
 }
