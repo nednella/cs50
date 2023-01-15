6d0ea8c97043a76653c697202c4f387c342d8fda
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-15 01:54:28 +0000

    Sun, Jan 15, 2023, 1:54 AM +00:00

--- test2.c
+++ test2.c
@@ -1,2 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+
+int main(void) {
+
+    // store the entered key from the CLI
+    string abc = "hello";
+    printf("%s\n", abc);
+    printf("%c\n", abc[0]);
+    printf("%c\n", abc[1]);
+    printf("%c\n", abc[2]);
+
+    string def = "bye";
+
+    abc[0] = def[0];
+
+
+}
 
