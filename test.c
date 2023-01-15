79159aa1a22480988a791ea18a79fce7232c5aee
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-15 00:54:50 +0000

    Sun, Jan 15, 2023, 12:54 AM +00:00

--- test.c
+++ test.c
@@ -5,38 +5,26 @@
 #include <ctype.h>
 
 
-	int main(void) {
+int main(void) {
 
-        // store the entered key from the CLI
-        string key = "zyxwvutsrqponmlkjihgfedcba";
+    // store the entered key from the CLI
+    string key = "zyxwvutsrqponmlkjihgfedcba";
 
-        // store the string length of the key
-        int keylength = strlen(key);
+    // store the string length of the key
+    int keylength = strlen(key);
 
+    printf("Key before sorting: %s", key);
+    printf("\n");
 
-
-
-
-        // iterate through the key 1 character at a time
-        for (int i = 0; i < keylength; i++) {
-
-            // check for non-alphabetic characters, terminate program if true
-            if (isalpha(key[i]) == 0) {
-                printf("The key must contain alphabetic characters only.\n");
-                return 1;
-            }
-
-            // bubble sort the key in ascdending order
-            for (int j = 0; j < (keylength - 1); j++) {
-
-                // compare 'char i' to 'char i + 1', if greater
-                if (key[j] > key[j+1]) {
-
-                    // store 'char i' temporarily
-                    char chTemp = key[j];
-                    key[j] = key[j + 1];
-                    key[j + 1] = chTemp;
-                }
+    // bubble sort key ascending order
+    for (int i = 0; i < (keylength - 1); i++) {
+        for (int j = (i + 1); j < keylength; j++) {
+            if (key[i] > key[j]) {
+                char ChTemp = key[i];
+                key[i] = key[j];
+                key[j] = ChTemp;
             }
         }
     }
+    printf("Key after sorting: %s", key);
+}
