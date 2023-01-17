b8c397e728c71f9da298de7e531de43dac99fb8a
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-17 18:12:54 +0000

    Tue, Jan 17, 2023, 6:12 PM +00:00

--- cs50/week2/substitution/substitution.c
+++ cs50/week2/substitution/substitution.c
@@ -34,8 +34,12 @@ int main(int argc, string argv[]) {
     // prompt user for plaintext
     string plaintext = get_string("Plain text: ");
 
+    // initialise an array for the text_conversion function to store the ciphertext
+    int arraylength = strlen(plaintext);
+    char ciphertext[arraylength + 1];
+
     // convert plaintext to ciphertext
-    string ciphertext = text_conversion(plaintext, key);
+    string ciphertext = text_conversion(plaintext, key, ciphertext);
 
 
 // ERROR HERE - array decay - local array declared inside function text_conversion ceases to exist once the function returns.
