324554b28cd50d2a0e8937fce47a00754e3d5c45
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-17 18:00:42 +0000

    Tue, Jan 17, 2023, 6:00 PM +00:00

--- cs50/week2/substitution/substitution.c
+++ cs50/week2/substitution/substitution.c
@@ -114,7 +114,7 @@ string text_conversion(string plaintext, string key) {
     int arraylength = strlen(plaintext);
 
     // create a character array of size equal to 'plaintext' to store the ciphered text, plus NULL value
-    char ciphertext[arraylength + 1] malloc(sizeof(ciphertext));
+    char ciphertext[arraylength + 1] //malloc(sizeof(ciphertext));
 
     // allocate dynamic memory for the ciphertext array
 
