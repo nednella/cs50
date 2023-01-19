ebf7aa2d2a46bf257ae39aaede19b6ca2db9fe06
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-19 00:19:09 +0000

    Thu, Jan 19, 2023, 12:19 AM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -141,7 +141,7 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < candidate_count; i++) {
 
         // check for candidate name matches in the voters preferences
-        if (strcmp(candidates[i], name) ==0) {
+        if (strcmp(candidates[i].name, name) ==0) {
 
             // if match, record the vote
             preferences[voter][rank] = i;
