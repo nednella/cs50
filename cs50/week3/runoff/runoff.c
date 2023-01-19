1fd1ed176fc15d8c3ffdcb129f3aab14fffc92a3
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-19 00:17:39 +0000

    Thu, Jan 19, 2023, 12:17 AM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -144,7 +144,7 @@ bool vote(int voter, int rank, string name)
         if (strcmp(candidates[i], name) ==0) {
 
             // if match, record the vote
-            preferences[voter] = i;
+            preferences[voter][rank] = i;
         }
     }
 
