b4453d89c68389f6e384a0f1b6c4754956f69828
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-26 20:37:47 +0000

    Thu, Jan 26, 2023, 8:37 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -3,23 +3,40 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // as a test, set every single pixel to BLACK rgbt[0, 0, 0]
+    // iterate through the columns
+    for (int i = 0; i < height; i++) {
 
-    // iterate through the rows
-    for (int i = 0; i < height, i++) {
+        // iterate through the rows
+        for (int j = 0; j < width; j++) {
 
-        //
+            //
 
-
-        // iterate through the columns
-        for (int j = 0; j < width, j++) {
-
-            // set all RGB values to 0 in each pixel within row i
-            image[i][j].rgbtRed = 0;
-            image[i][j].rgbtGreen = 0;
-            image[i][j].rgbtBlue = 0;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // as a test, set every single pixel to BLACK rgbt[0, 0, 0]
+    // iterate through the rows
+    //for (int i = 0; i < height; i++) {
+        // iterate through the columns
+        //for (int j = 0; j < width; j++) {
+            // set all RGB values to 0 in each pixel within row i
+            //image[i][j].rgbtRed = 0;
+            //image[i][j].rgbtGreen = 0;
+            //image[i][j].rgbtBlue = 0;
+       //}
+    //}
     return;
 }
 
