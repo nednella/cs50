92c5b02da1422903897ea83806e897bd7ab1f509
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-26 21:00:12 +0000

    Thu, Jan 26, 2023, 9:00 PM +00:00

--- test.c
+++ test.c
@@ -3,41 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 
 int main(void) {
 
-    // store the entered key from the CLI
-    string key = "zyxwvutsrqponmlkjihgfedcbz";
-    printf("%s\n", key);
+    int R = 255;
+    int G = 243;
+    int B = 17;
 
-    // store the string length of the key
-    int keylength = strlen(key);
+    float avgRGB = ((R + G + B) / 3);
+    printf("%f\n", avgRGB);
 
-    // create a character array of size equal to 'key'
-    char key_modified[keylength + 1];
-
-    // convert key to UPPERCASE
-    for (int i = 0; i < keylength; i++) {
-        key_modified[i] = toupper(key[i]);
-        printf("%c", key_modified[i]);
-    }
-    printf("\n");
-
-    // check for unique characters
-    for (int i = 0; i < (keylength - 1); i++) {
-
-        // initiate staggered counter
-        for (int j = (i + 1); j < keylength; j++) {
-
-            // if 'char i' matches 'char i + 1'
-            if (key_modified[i] == key_modified[j]) {
-
-                // key check failed
-                printf("Key is not unique.\n");
-                return 1;
-            }
-        }
-    }
-    printf("Key is unique!\n");
+    int gr = round(avgRGB);
+    printf ("%i\n", gr);
 }
