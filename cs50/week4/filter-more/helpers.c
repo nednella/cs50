8d98254e105c2566b4deec51eba5509cc1a0b83c
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-27 16:45:01 +0000

    Fri, Jan 27, 2023, 4:45 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -83,7 +83,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     // in this case, want to interact with EVERY pixel in the image
 
     // initialise temporary image
-    RGBTRIPLE tmp[height][width] = malloc(sizeof())
+    RGBTRIPLE tmp[height][width] = malloc((height*width)*sizeof(int))
 
     // iterate through the columns
     for (int i = 0; i < height; i++) {
