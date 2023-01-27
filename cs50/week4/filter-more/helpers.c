0948f2ad5c22ee483c055fabcc8e3246201b2c3f
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-27 19:25:46 +0000

    Fri, Jan 27, 2023, 7:25 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -205,8 +205,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++) {
 
             image[i][j] = copy[i][j];
-            image[i][j] = copy[i][j];
-            image[i][j] = copy[i][j];
+
         }
      }
     return;
