81ea4bfe5105a22714824debee4b71c400ecfb7c
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-27 18:25:42 +0000

    Fri, Jan 27, 2023, 6:25 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -28,17 +28,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtBlue = round(avgRGB);
         }
     }
-    // as a test, set every single pixel to BLACK rgbt[0, 0, 0]
-    // iterate through the rows
-    //for (int i = 0; i < height; i++) {
-        // iterate through the columns
-        //for (int j = 0; j < width; j++) {
-            // set all RGB values to 0 in each pixel within row i
-            //image[i][j].rgbtRed = 0;
-            //image[i][j].rgbtGreen = 0;
-            //image[i][j].rgbtBlue = 0;
-       //}
-    //}
     return;
 }
 
