ad144cd6e43afd041decf516521212307562f961
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-30 19:02:36 +0000

    Mon, Jan 30, 2023, 7:02 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -109,19 +109,87 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
             // for the selected pixel [i][j]
             // check if top left corner is valid
-            if (i - 1 >= 0 && j - 1 >= 0)
-
-            // check if top middle is valid
-            if (i - 1 >= 0)
-
-
-
-                }
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                sum_pixel ++;
+                sum_R += image[i - 1][j - 1].rgbtRed;
+                sum_G += image[i - 1][j - 1].rgbtBlue;
+                sum_B += image[i - 1][j - 1].rgbtGreen;
             }
 
+            // check if top middle is valid
+            if (i - 1 >= 0) {
+                sum_pixel ++;
+                sum_R += image[i - 1][j].rgbtRed;
+                sum_G += image[i - 1][j].rgbtBlue;
+                sum_B += image[i - 1][j].rgbtGreen;
+            }
 
+            // check if top right is valid
+            if (i - 1 >= 0 && j < width) {
+                sum_pixel ++;
+                sum_R += image[i - 1][j + 1].rgbtRed;
+                sum_G += image[i - 1][j + 1].rgbtBlue;
+                sum_B += image[i - 1][j + 1].rgbtGreen;
+            }
 
+            // check if middle left is valid
+            if (j - 1 >= 0) {
+                sum_pixel ++;
+                sum_R += image[i][j - 1].rgbtRed;
+                sum_G += image[i][j - 1].rgbtBlue;
+                sum_B += image[i][j - 1].rgbtGreen;
+            }
 
+            // CURRENT PIXEL
+            sum_pixel ++;
+            sum_R += image[i][j].rgbtRed;
+            sum_G += image[i][j].rgbtBlue;
+            sum_B += image[i][j].rgbtGreen;
+
+            // check if middle right is valid
+            if (j < width) {
+                sum_pixel ++;
+                sum_R += image[i][j + 1].rgbtRed;
+                sum_G += image[i][j + 1].rgbtBlue;
+                sum_B += image[i][j + 1].rgbtGreen;
+            }
+
+            // check if bottom left is valid
+            if (i + 1 < height && j >= 0) {
+                sum_pixel ++;
+                sum_R += image[i + 1][j - 1].rgbtRed;
+                sum_G += image[i + 1][j - 1].rgbtBlue;
+                sum_B += image[i + 1][j - 1].rgbtGreen;
+            }
+
+            // check if bottom middle is valid
+            if (i + 1 < height) {
+                sum_pixel ++;
+                sum_R += image[i + 1][j].rgbtRed;
+                sum_G += image[i + 1][j].rgbtBlue;
+                sum_B += image[i + 1][j].rgbtGreen;
+            }
+
+            // check if bottom right is valid
+            if (i + 1 < height && j + 1 < width) {
+                sum_pixel ++;
+                sum_R += image[i + 1][j + 1].rgbtRed;
+                sum_G += image[i + 1][j + 1].rgbtBlue;
+                sum_B += image[i + 1][j + 1].rgbtGreen;
+            }
+
+            // average out the summed RGB values
+            int avg_R = round(sum_R/sum_pixel);
+            int avg_G = round(sum_G/sum_pixel);
+            int avg_B = round(sum_B/sum_pixel);
+
+            // replace the copied images' [i][j] pixel RGB values with the new averaged RGV values
+            copy[i][j].rgbtRed = avg_R;
+            copy[i][j].rgbtBlue = avg_G;
+            copy[i][j].rgbtGreen = avg_B;
+
+         }
+    }
 
 
     // swap in copied image (blurred pixels) to the real image
