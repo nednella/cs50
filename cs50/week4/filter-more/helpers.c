622b2a9bbb8c1dfdb8e5cb9f86291c529ca04449
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-30 18:49:15 +0000

    Mon, Jan 30, 2023, 6:49 PM +00:00

--- cs50/week4/filter-more/helpers.c
+++ cs50/week4/filter-more/helpers.c
@@ -101,50 +101,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
             // initialise counters (inside of loop so they are reset for each pixel!)
             double sum_pixel = 0;
-            double sum_RGB[] = {0, 0, 0};
+            double sum_R = 0;
+            double sum_G = 0;
+            double sum_B = 0;
 
-            // loop through 3x3 block around [i][j] and check if valid
-            // row iteration
-            for (int k = (i - 1); k <= (i + 1); k++) {
+            //double sum_RGB[] = {0, 0, 0};
 
-                // column iteration
-                for (int l = (j - 1); l <= (j + 1); j++) {
+            
 
-                    // check if pixel is within valid range of image
-                    if (k < 0 || k >= height || l < 0 || l >= width) {
-                        continue;
-                    }
-                    sum_pixel ++;
-                    sum_RGB[0] += image[k][l].rgbtRed;
-                    sum_RGB[1] += image[k][l].rgbtBlue;
-                    sum_RGB[2] += image[k][l].rgbtGreen;
 
-                    //if ((k >= 0) && (k < height) && (l >= 0) && (l < width)) {
-
-                        // count pixel and RGB values (from the ORIGINAL IMAGE)
-                        //sum_pixel ++;
-                        //sum_RGB[0] += image[k][l].rgbtRed;
-                        //sum_RGB[1] += image[k][l].rgbtBlue;
-                        //sum_RGB[2] += image[k][l].rgbtGreen;
-                    //}
-
-                    //else {
-                        //continue;
-                    //}
                 }
             }
 
-            // calculate average RGB values of the present pixels
-            int avg_R = round(sum_RGB[0]/sum_pixel);
-            int avg_G = round(sum_RGB[1]/sum_pixel);
-            int avg_B = round(sum_RGB[2]/sum_pixel);
 
-            // set COPY IMAGE pixel [i][j]'s RGB values to average of the surrounding pixels
-            copy[i][j].rgbtRed = (avg_R);
-            copy[i][j].rgbtGreen = (avg_G);
-            copy[i][j].rgbtBlue = (avg_B);
-        }
-    }
+
+
+
 
     // swap in copied image (blurred pixels) to the real image
     for (int i = 0; i < height; i++) {
@@ -152,6 +124,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             image[i][j] = copy[i][j];
         }
     }
+
     free(copy);
     return;
 }
--- test2.c
+++ test2.c
@@ -7,16 +7,61 @@
 
 int main(void) {
 
-    // store the entered key from the CLI
-    string abc = "hello";
-    printf("%s\n", abc);
-    printf("%c\n", abc[0]);
-    printf("%c\n", abc[1]);
-    printf("%c\n", abc[2]);
+    // iterate through the columns
+    for (int i = 0; i < height; i++) {
 
-    string def = "bye";
+        // iterate through the rows
+        for (int j = 0; j < width; j++) {
 
-    abc[0] = def[0];
+            // copy original pixel to a COPY image (to allow editing of RGB values without affecting the original)
+            copy[i][j] = image[i][j];
+
+            // initialise counters (inside of loop so they are reset for each pixel!)
+            double sum_pixel = 0;
+            double sum_RGB[] = {0, 0, 0};
+
+            // loop through 3x3 block around [i][j] and check if valid
+            // row iteration
+            for (int k = (i - 1); k <= (i + 1); k++) {
+
+                // column iteration
+                for (int l = (j - 1); l <= (j + 1); j++) {
+
+                    // check if pixel is within valid range of image
+                    if (k < 0 || k >= height || l < 0 || l >= width) {
+                        continue;
+                    }
+                    sum_pixel ++;
+                    sum_RGB[0] += image[k][l].rgbtRed;
+                    sum_RGB[1] += image[k][l].rgbtBlue;
+                    sum_RGB[2] += image[k][l].rgbtGreen;
+
+                    //if ((k >= 0) && (k < height) && (l >= 0) && (l < width)) {
+
+                        // count pixel and RGB values (from the ORIGINAL IMAGE)
+                        //sum_pixel ++;
+                        //sum_RGB[0] += image[k][l].rgbtRed;
+                        //sum_RGB[1] += image[k][l].rgbtBlue;
+                        //sum_RGB[2] += image[k][l].rgbtGreen;
+                    //}
+
+                    //else {
+                        //continue;
+                    //}
+                }
+            }
+
+            // calculate average RGB values of the present pixels
+            int avg_R = round(sum_RGB[0]/sum_pixel);
+            int avg_G = round(sum_RGB[1]/sum_pixel);
+            int avg_B = round(sum_RGB[2]/sum_pixel);
+
+            // set COPY IMAGE pixel [i][j]'s RGB values to average of the surrounding pixels
+            copy[i][j].rgbtRed = (avg_R);
+            copy[i][j].rgbtGreen = (avg_G);
+            copy[i][j].rgbtBlue = (avg_B);
+        }
+    }
 
 
 }
