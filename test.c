76b6cdbf6703d1ffad089e63c12606d8d1ce1aae
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-30 17:24:26 +0000

    Mon, Jan 30, 2023, 5:24 PM +00:00

--- test.c
+++ test.c
@@ -4,159 +4,45 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdint.h>
 
+typedef uint8_t  BYTE;
+
+typedef struct
+{
+    BYTE  R;
+    BYTE  G;
+    BYTE  B;
+}
+RGBTRIPLE;
 
 int main(void) {
 
-    double R = 255;
-  double  G = 243;
-    double B = 17;
+    int height = 2;
+    int width = 2;
 
-    double avgRGB = (float) ((R + G + B) / 3);
-    printf("%f\n", avgRGB);
+    RGBTRIPLE *image[height][width] = malloc(sizeof(RGBTRIPLE) * height * width);
 
-    int gr = round(avgRGB);
-    printf ("%i\n", gr);
+    image[0][0].R = 100;
+    image[0][0].G = 100;
+    image[0][0].B = 100;
 
+    image[0][1].R = 120;
+    image[0][1].G = 120;
+    image[0][1].B = 120;
 
+    image[1][0].R = 160;
+    image[1][0].G = 160;
+    image[1][0].B = 160;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // loop through 3x3 of [i][j] and check if valid
-            // row iteration
-            for (int k = (i - 1); k <= (i + 1); k++) {
-
-                // column iteration
-                for (int l = (j - 1); l <= (j + 1); j++) {
-
-                    // check if pixel is within valid range of image
-                    if ((k >= 0) && (k < height) && (l >= 0) && (l < width)) {
-
-                        // count pixel and RGB values (from the ORIGINAL IMAGE)
-                        sum_pixel ++;
-                        sum_RGB[0] += image[k][l].rgbtRed;
-                        sum_RGB[1] += image[k][l].rgbtBlue;
-                        sum_RGB[2] += image[k][l].rgbtGreen;
-                    }
-
-                    else {
-                        continue;
-                    }
-                }
-            }
+    image[1][1].R = 180;
+    image[1][1].G = 180;
+    image[1][1].B = 180;
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-            // take RGB values of ALL pixels within 3x3 of [i][j], assuming pixels exist
-
-            // current pixel
-            sum_pixel ++;
-            sum_RGB[0] += image[i][j].rgbtRed;
-            sum_RGB[1] += image[i][j].rgbtGreen;
-            sum_RGB[2] += image[i][j].rgbtBlue;
-
-            // if upper left exists
-            if ((i - 1) >= 0 && (j - 1) >= 0) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i - 1][j - 1].rgbtRed;
-                sum_RGB[1] += image[i - 1][j - 1].rgbtBlue;
-                sum_RGB[2] += image[i - 1][j - 1].rgbtGreen;
-
-            }
-
-            // if upper middle exists
-            if ((i - 1) >= 0) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i - 1][j].rgbtRed;
-                sum_RGB[1] += image[i - 1][j].rgbtBlue;
-                sum_RGB[2] += image[i - 1][j].rgbtGreen;
-            }
-
-            // if upper right exists
-            if ((i - 1) >= 0 && (j + 1) < width) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i - 1][j + 1].rgbtRed;
-                sum_RGB[1] += image[i - 1][j + 1].rgbtBlue;
-                sum_RGB[2] += image[i - 1][j + 1].rgbtGreen;
-            }
-
-            // if middle left exists
-            if ((j - 1) >= 0) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i][j - 1].rgbtRed;
-                sum_RGB[1] += image[i][j - 1].rgbtBlue;
-                sum_RGB[2] += image[i][j - 1].rgbtGreen;
-            }
-
-            // if middle right exists
-            if ((j + 1) < width) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i][j + 1].rgbtRed;
-                sum_RGB[1] += image[i][j + 1].rgbtBlue;
-                sum_RGB[2] += image[i][j + 1].rgbtGreen;
-            }
-
-            // lower left
-            if ((i + 1) < height && (j - 1) >= 0) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i + 1][j - 1].rgbtRed;
-                sum_RGB[1] += image[i + 1][j - 1].rgbtBlue;
-                sum_RGB[2] += image[i + 1][j - 1].rgbtGreen;
-            }
-
-            // lower middle
-            if ((i + 1) < height) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i + 1][j].rgbtRed;
-                sum_RGB[1] += image[i + 1][j].rgbtBlue;
-                sum_RGB[2] += image[i + 1][j].rgbtGreen;
-            }
-
-            // lower right
-            if((i + 1) < height && (j + 1) < width) {
-
-                sum_pixel ++;
-
-                sum_RGB[0] += image[i + 1][j + 1].rgbtRed;
-                sum_RGB[1] += image[i + 1][j + 1].rgbtBlue;
-                sum_RGB[2] += image[i + 1][j + 1].rgbtGreen;
-            }
 
 }
