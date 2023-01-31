1039beb2792490ecd6d8737da1d6617d556be4eb
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-31 23:54:23 +0000

    Tue, Jan 31, 2023, 11:54 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -29,10 +29,10 @@ int main(int argc, char *argv[])
 
 
     // initialise the buffer
-    BYTE buffer[BLOCK];// = malloc (BLOCK * sizeof(BYTE));
+    BYTE buffer[512];
 
     // while there is  >= 1 block of data remaining, load the data into memory with a pointer to the first address of the block, named buffer
-    while (fread(&buffer, 1, BLOCK, file) == BLOCK) {
+    while (fread(buffer, 1, BLOCK, file) == BLOCK) {
 
         // iterate through the buffer
         for (int i = 0; i < BLOCK; i++) {
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
                 if (firstimage) {
                     // create a new JPEG file
-                    sprintf(&buffer, "%03i.jpg", imagecount);
+                    sprintf(buffer, "%03i.jpg", imagecount);
                     firstimage = false;
                     imagecount ++;
                 }
