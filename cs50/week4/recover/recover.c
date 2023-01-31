cd6aab5210e4d96def263b19b3f909531c43bc47
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-31 20:05:38 +0000

    Tue, Jan 31, 2023, 8:05 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     // check that CLI was used correctly
-    if (argc == 0 || argc > 1) {
+    if (!argc == 1) {
         printf("Usage: ./recover [IMAGE]\n");
         return 1;
     }
