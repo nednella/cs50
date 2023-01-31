534f8397afb8dfc73e2c2d0c3c3e4c60a5f5ea64
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-31 19:17:31 +0000

    Tue, Jan 31, 2023, 7:17 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -7,17 +7,22 @@ int main(int argc, char *argv[])
 
 
     // if yes, open the image in question
+    char *filename = argv[];
 
+    FILE *f = fopen(filename, "r"); // open file in read mode
 
-    // load the image
-
-    // store the information?
 
     // start searching data from NULL -> END, for the beginning of a jpeg
+    // key for a JPEG file header...
+        // 1st byte always 0xff
+        // 2nd byte always 0xd8
+        // 3rd byte always 0xff
+        // 4th byte always 0xeX... (where X is 0 - F in hexadecimal)
 
-    // open a new JPEG file
 
-    // keep writing data in chunks of 512 bytes until a new JPEG file is reached
+    // once header block is found, open a new JPEG file
+
+    // keep writing data in chunks of 512 bytes until a new JPEG file header is reached
 
     // close old file
 
