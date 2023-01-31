0f5e4838280fcc2856c44bbad08cf51de4578cd8
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-31 19:13:00 +0000

    Tue, Jan 31, 2023, 7:13 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -11,7 +11,16 @@ int main(int argc, char *argv[])
 
     // load the image
 
-
     // store the information?
+
+    // start searching data from NULL -> END, for the beginning of a jpeg
+
+    // open a new JPEG file
+
+    // keep writing data in chunks of 512 bytes until a new JPEG file is reached
+
+    // close old file
+
+    // repeat
 
 }
