beaf8ea98d836d2fa9dddc0a31d898757cd18cfc
benallenden <119449811+benallenden@users.noreply.github.com>
2023-01-31 19:10:47 +0000

    Tue, Jan 31, 2023, 7:10 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -3,5 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    // check that CLI was used correctly
+
+
+    // if yes, open the image in question
+
+
+    // load the image
+
+
+    // store the information?
 
 }
