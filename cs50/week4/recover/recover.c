1c597bfd7dab847a82c71ee4c8b0dc73a81ba32a
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-01 00:04:24 +0000

    Wed, Feb 1, 2023, 12:04 AM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 
 typedef uint8_t BYTE;
-
 const int BLOCK = 512;
 
 // global variables to help decide what actions to take when encountering a new JPEG file in the loaded file
@@ -29,7 +28,7 @@ int main(int argc, char *argv[])
 
 
     // initialise the buffer
-    BYTE buffer[512];
+    BYTE buffer[BLOCK];
 
     // while there is  >= 1 block of data remaining, load the data into memory with a pointer to the first address of the block, named buffer
     while (fread(buffer, 1, BLOCK, file) == BLOCK) {
@@ -42,7 +41,7 @@ int main(int argc, char *argv[])
 
                 if (firstimage) {
                     // create a new JPEG file
-                    sprintf(      , "%03i.jpg", imagecount);
+                    //sprintf(      , "%03i.jpg", imagecount);
                     firstimage = false;
                     imagecount ++;
                 }
@@ -51,14 +50,14 @@ int main(int argc, char *argv[])
 
 
                     // create new JPEG file
-                    sprintf(      , "%03i,jpg", imagecount);
+                    //sprintf(      , "%03i,jpg", imagecount);
                 }
             }
         }
     }
 
 
-    
+
     // repeat until end of the card:
         // read 512 bytes into a buffer
         // iterate through buffer
