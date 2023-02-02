c87fc5baaa74eb2d7ae20710da9db34120e32192
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 20:20:18 +0000

    Thu, Feb 2, 2023, 8:20 PM +00:00

--- cs50/week5/inheritance/inheritance.c
+++ cs50/week5/inheritance/inheritance.c
@@ -90,19 +90,17 @@ person *create_family(int generations)
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
-    // TODO: Handle base case
-    if (*p == NULL) {
+    // Handle base case
+    if (p == NULL) {
         return;
     }
 
-    // TODO: Free parents recursively
-    else {
-        free_family(p->parents[0]);
-        free_family(p->parents[1]);
-    }
+    // Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
-    // TODO: Free child
-
+    // Free child
+    free(p);
 }
 
 
