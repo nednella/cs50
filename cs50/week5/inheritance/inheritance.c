196adbc66cf534ae6d56def9e43db201a0539b5c
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 20:15:45 +0000

    Thu, Feb 2, 2023, 8:15 PM +00:00

--- cs50/week5/inheritance/inheritance.c
+++ cs50/week5/inheritance/inheritance.c
@@ -74,7 +74,6 @@ person *create_family(int generations)
         p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        char alleles[] = {'A', 'B', 'O'};
         p->alleles[0] = random_allele();
         p->alleles[1] = random_allele();
 
