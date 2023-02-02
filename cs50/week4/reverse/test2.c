f4060b56ca0971950555e872cc69d93129f38c9f
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 01:32:27 +0000

    Thu, Feb 2, 2023, 1:32 AM +00:00

--- cs50/week4/reverse/test2.c
+++ cs50/week4/reverse/test2.c
@@ -9,18 +9,19 @@
 
 int main(void)
 {
-    int block_size = 4;
     int buffer[] = {1, 2, 3, 4};
     int temp[] = {0, 0, 0, 0};
 
+    int arr_size = sizeof(buffer) / sizeof(buffer[0]);
+
     // store buffer in reverse, into temporary
-    for (int i = block_size, j = 0; i >= 0; i--, j++) {
+    for (int i = arr_size, j = 0; i >= 0; i--, j++) {
 
             temp[j] = buffer[i];
         }
 
     // print temporary array in full
-    for (int i = 0; i < block_size; i++) {
+    for (int i = 0; i < arr_size; i++) {
         printf("%i\n", temp[i]);
     }
 
