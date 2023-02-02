4b3ea173ec8cc73b6adcc13a1a75fc8bf6970a89
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 01:53:07 +0000

    Thu, Feb 2, 2023, 1:53 AM +00:00

--- cs50/week4/reverse/reverse.c
+++ cs50/week4/reverse/reverse.c
@@ -12,21 +12,18 @@ int get_block_size(WAVHEADER header);
 int main(int argc, char *argv[])
 {
     // Ensure proper usage
-    // TODO #1
     if (!(argc == 3)) {
         printf("Usage: ./reverse [input.wav] [output.wav]");
         return 1;
     }
 
     // Open input file for reading
-    // TODO #2
     FILE *input = fopen(argv[1], "r");
     if (input == NULL) {
         printf("Error: file not found.\n");
     }
 
     // Read header into an array
-    // TODO #3
     // allocate memory to store the file header
     WAVHEADER *header = malloc(44 * sizeof(BYTE));
 
@@ -35,14 +32,12 @@ int main(int argc, char *argv[])
     int header_offset = ftell(input); // stream position at the end of the header block (should be equal to 44th byte)
 
     // Use check_format to ensure WAV format
-    // TODO #4
     if (check_format(*header) == false) {
         printf("Input is not a WAV file.");
         return 1;
     }
 
     // Open output file for writing
-    // TODO #5
     // allocate memory for the name of the user-specified output file
     int length = strlen(argv[2]);
     char filename[length];
@@ -57,48 +52,12 @@ int main(int argc, char *argv[])
     output = fopen(filename, "w");
 
     // Write header to file
-    // TODO #6
     fwrite(header, 1, 44, output);
 
     // Use get_block_size to calculate size of block
-    // TODO #7
     int block_size = get_block_size(*header);
 
-    // Write reversed audio to file - ONLY INTERESTED IN 45TH BYTE ONWARDS
-    // TODO #8
-
-    // initialise a buffer to store each block of audio data
-    BYTE buffer[block_size];
-    //BYTE temp[block_size];
-
-    // By downloading input.wav, the file properties tell me the file size is 352,844 bytes!
-    // set stream pointer to end of file, then move back by 1 * block_size
-    (void) fseek(input, -(block_size), SEEK_END); // current position is now 352,840
-    //int current_pos = ftell(input);
-    //printf("Here...\n");
-    //printf("%i\n", current_pos);
-
-    // loop through the data in reverse, 1 block at a time, stopping when we reach the files' header metadata
-    while(ftell(input) > header_offset) {
-
-        // read into the buffer from current stream position, for a total of block_size bytes
-        fread(buffer, 1, block_size, input);
-
-        // reverse the data in buffer and store it into temporary
-        //for (int i = block_size - 1, j = 0; i >= 0; i--, j++) { // --- ERROR HERE, must consider channels 1 and 2 when reversing the data
-
-            //temp[j] = buffer[i];
-        //}
-
-        // write the reversed data into the output file
-        fwrite(buffer, 1, block_size, output);
-
-        // move the stream pointer back by 2*block_size
-        (void) fseek(input, -(2 * block_size), SEEK_CUR);
-    }
-
-
-
+    // Write reversed audio to file
 
 
     // initialise a buffer the size of 1 block of audio data
@@ -111,23 +70,48 @@ int main(int argc, char *argv[])
             // move stream pointer back by 2*block_size
 
 
+    // initialise a buffer to store the size of 1 block of audio data
+    BYTE buffer[block_size];
+    BYTE temp[block_size];
+
+    // set stream pointer to end of file, then move back by 1 * block_size
+    // by downloading input.wav, the file properties tell me the file size is 352,844 bytes!
+    (void) fseek(input, -(block_size), SEEK_END); // current position is now 352,840
+
+    // loop through the data in reverse, 1 block at a time, stopping when we reach the files' header metadata
+    while(ftell(input) > header_offset) {
+
+        // read into the buffer from current stream position, for a total of block_size bytes
+        fread(buffer, 1, block_size, input);
+
+
+
+
+        // reverse the data in buffer and store it into temporary
+        //for (int i = block_size - 1, j = 0; i >= 0; i--, j++) { // --- ERROR HERE, must consider channels 1 and 2 when reversing the data
+
+            //temp[j] = buffer[i];
+        //}
 
 
 
 
 
+        // write the reversed data into the output file
+        fwrite(buffer, 1, block_size, output);
 
-
-
-
-
-
-
+        // move the stream pointer back by 2*block_size
+        (void) fseek(input, -(2 * block_size), SEEK_CUR);
+    }
 
     fclose(input);
     fclose(output);
     return 0;
 }
+
+
+
+
 
 bool check_format(WAVHEADER header)
 {
@@ -141,7 +125,6 @@ bool check_format(WAVHEADER header)
 
 int get_block_size(WAVHEADER header)
 {
-    // TODO #7
     // access header, and determine the block size (no. of channels * bits per sample)
     int channels = header.numChannels;
     int bps = header.bitsPerSample / 8;
