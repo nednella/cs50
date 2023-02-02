b37f70dde0f731dc7648db11e2947fb716e32e8b
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 16:38:18 +0000

    Thu, Feb 2, 2023, 4:38 PM +00:00

--- cs50/week4/smiley/helpers.c
+++ cs50/week4/smiley/helpers.c
@@ -2,8 +2,7 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
-    // choice colour
+    // choice colour - CYAN
     int R_value = 0;
     int G_value = 100;
     int B_value = 100;
