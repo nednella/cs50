79e0a36baa4ab0ab49aa5096d392233adfff0e3d
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 16:34:05 +0000

    Thu, Feb 2, 2023, 4:34 PM +00:00

--- cs50/week4/smiley/helpers.c
+++ cs50/week4/smiley/helpers.c
@@ -3,4 +3,21 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    // choice colour
+    int R_value = 0;
+    int G_value = 100;
+    int B_value = 100;
+
+    // loop through every pixel in the image
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+
+            // for each pixel, access the RGB values and set them to a colour of choice
+            image[i][j].rgbtRed = R_value;
+            image[i][j].rgbtGreen = G_value;
+            image[i][j].rgbtBlue = B_value;
+        }
+    }
+    
+    return;
 }
