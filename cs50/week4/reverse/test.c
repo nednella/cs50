93a8bac793402409636559fc5b244bb1aeb3a4f2
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 00:49:58 +0000

    Thu, Feb 2, 2023, 12:49 AM +00:00

--- cs50/week4/reverse/test.c
+++ cs50/week4/reverse/test.c
@@ -5,83 +5,18 @@
 
 #include "wav.h"
 
-bool check_format(WAVHEADER header);
-int get_block_size(WAVHEADER header);
 
-int main(int argc, char *argv[])
+
+int main(void)
 {
-    // Ensure proper usage
-    // TODO #1
-    if (!(argc == 3)) {
-        printf("Usage: ./reverse [input.wav] [output.wav]");
+    FILE *text = fopen("text.txt", "r");
+    if (text == NULL) {
+        printf("Error: unable to open file.");
         return 1;
     }
 
-    // Open input file for reading
-    // TODO #2
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL) {
-        printf("Error: file not found.\n");
-    }
+    // initialise a buffer to store the data from text.txt
+    
 
-    // Read header into an array
-    // TODO #3
-    // allocate memory to store the file header
-    WAVHEADER header;
-
-    // read the input file to extract the header
-    fread(header, 1, 44, input); // file header is 44 bytes in total
-
-    // Use check_format to ensure WAV format
-    // TODO #4
-    if (check_format(header) == false) {
-        printf("Input is not a WAV file.");
-        return 1;
-    }
-
-    // Open output file for writing
-    // TODO #5
-    // allocate memory for the name of the user-specified output file
-    int length = strlen(argv[2]);
-    char filename = malloc(length * sizeof(char));
-
-    // initialise the output file
-    FILE *output;
-
-    // create a new WAV file for output
-    sprintf(filename, "%s", argv[2]);
-
-    // open the output file
-    output = fopen(filename, "w");
-
-
-
-
-    // Write header to file
-    // TODO #6
-
-    // Use get_block_size to calculate size of block
-    // TODO #7
-
-    // Write reversed audio to file
-    // TODO #8
-
-    fclose(file);
-    return 0;
-}
-
-bool check_format(WAVHEADER header)
-{
-    // access WAVHEADER, specifically the format array, and check if the chars W, A, V and E are stored in this array
-    if (!(WAVHEADER.format[0] == "W" && WAVHEADER.format[1] == "A" && WAVHEADER.format[2] == "V" && WAVHEADER.format[3] == "E")) {
-        return false;
-    }
-
-    return true;
-}
-
-int get_block_size(WAVHEADER header)
-{
-    // TODO #7
     return 0;
 }
