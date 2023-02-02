efecaefcfa31a0a1318af66b1ebcde52ed239be9
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 19:24:43 +0000

    Thu, Feb 2, 2023, 7:24 PM +00:00

--- cs50/week5/inheritance/test.c
+++ cs50/week5/inheritance/test.c
@@ -8,16 +8,17 @@ int main(void)
 {
     // Seed random number generator
     srand(time(0));
-    
+
     char parent1[] = {'A', 'B'};
     char parent2[] = {'O', 'A'};
 
     char child[2];
 
+    // use a random number generator to pick a 1 blood type from each parent
     child[0] = parent1[rand() % 2];
     child[1] = parent2[rand() % 2];
 
-    printf("%c\n", child[0]);
+    printf("%c", child[0]);
     printf("%c\n", child[1]);
 
 }
