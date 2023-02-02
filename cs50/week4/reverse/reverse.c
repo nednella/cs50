69d55fafa5804601e84820e10c728c2a0d9fd22f
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 01:07:01 +0000

    Thu, Feb 2, 2023, 1:07 AM +00:00

--- cs50/week4/reverse/reverse.c
+++ cs50/week4/reverse/reverse.c
@@ -71,7 +71,8 @@ int main(int argc, char *argv[])
     //BYTE buffer[block_size];
 
     // set stream pointer to end of file, then move back by 1 * block_size
-    int current_pos = fseek(input, 0, SEEK_END);
+    (void) fseek(input, -1, SEEK_END);
+    int current_pos = ftell(input);
     printf("Here...\n");
     printf("%i\n", current_pos);
 
