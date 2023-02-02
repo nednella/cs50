04114683394cdb2a6fc57ea04c4a9154a793c1c8
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 00:05:24 +0000

    Thu, Feb 2, 2023, 12:05 AM +00:00

--- cs50/week4/reverse/reverse.c
+++ cs50/week4/reverse/reverse.c
@@ -57,6 +57,7 @@ int main(int argc, char *argv[])
 
     // Write header to file
     // TODO #6
+    fwrite(header, 1, 44, output);
 
     // Use get_block_size to calculate size of block
     // TODO #7
