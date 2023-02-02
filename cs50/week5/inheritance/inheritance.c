2f4b800a875b9e9e01518f31c7e591ff7a23e782
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-02 18:05:19 +0000

    Thu, Feb 2, 2023, 6:05 PM +00:00

--- cs50/week5/inheritance/inheritance.c
+++ cs50/week5/inheritance/inheritance.c
@@ -44,6 +44,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *f_member = malloc(sizeof());
 
     // If there are still generations left to create
     if (generations > 1)
