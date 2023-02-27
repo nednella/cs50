e925817d701be2131cd33e3db469bae62a95bf60
benallenden <119449811+benallenden@users.noreply.github.com>
2023-02-27 21:28:28 +0000

    Mon, Feb 27, 2023, 9:28 PM +00:00

--- cs50/week5/speller/dictionary.c
+++ cs50/week5/speller/dictionary.c
@@ -116,8 +116,8 @@ bool load(const char *dictionary) {
         }
 
         wordcount++;                            // count the word being read - string is buffered whilst evaluating while condition!
-        strcpy(temp->word, buffer);             // copy word into newly created temp node
-        unsigned int index = hash(temp->word);  // obtain a hash value for the given word
+        strcpy(temp->word, buffer);             // write word to new node
+        unsigned int index = hash(temp->word);  // obtain a hash value for word
 
         // check if first element in the linked list
         if (table[index] == NULL) {             // if true
