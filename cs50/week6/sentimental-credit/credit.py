8c7464896d3e04dc41aa8f11b531a9aa392e0a85
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 20:40:41 +0000

    Tue, Mar 7, 2023, 8:40 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -6,16 +6,14 @@ def main():
 
     card = int(input("Card Number: "))              # prompt user for card number
 
-    length = int(math.log10(card)) + 1              # determine card length
-    #if valid_length(length) == False:               # check if length valid
+
+    #if valid_length(card) == False:                 # check if length valid
         #print("INVALID")
         #sys.exit(1)
 
-    if luhn_alg(card)
-
-    digits = return_list(card)
-
-
+    if luhn_algorithm(card) == False:                     # check if card number valid
+        print("INVALID")
+        sys.exit(1)
 
 
 
@@ -23,27 +21,26 @@ def main():
 
 
 
-
-
-
-
-
-# check for valid card length (13 to 16 digits)
-def valid_length(length):
-    if (13 <= length <= 16):
+def valid_length(card):
+    length = int(math.log10(card)) + 1              # determine card length
+    if (13 <= length <= 16):                        # check for valid card length (13 to 16 digits)
         return True
     else:
         return False
 
 
 # obtain credit card in indexable list form
-def return_list(card):
-    tmp = list(map(int, str(card)))
-    return tmp
+#def return_list(card):
+    #tmp = list(map(int, str(card)))
+    #return tmp
 
 
-
-
+# perform Luhn's algorithm check on the card
+def luhn_algorithm(card):
+    card_digits = list(map(int, str(card)))         # convert card to an indexable list
+    length = int(math.log10(card)) + 1              # determine card length
+    for i in range(length):
+        print(card_digits[i])
 
 
 main()
