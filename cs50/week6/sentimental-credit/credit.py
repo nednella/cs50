d1919c67d623583d61cfb29f35f7cd4dae908f2d
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 20:55:25 +0000

    Tue, Mar 7, 2023, 8:55 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -40,8 +40,8 @@ def luhn_algorithm(card):
     digits = list(map(int, str(card)))              # convert credit card digits to an indexable list
     #digits = digits.reverse                         # set the list in reverse
 
-    luhn_sum = 0                                    # initialise algorithm summation
-    for i in range(0, (len(digits) + 1) / 2, 2):
+    #luhn_sum = 0                                    # initialise algorithm summation
+    for i in range(len(digits) / 2): #range(0, (len(digits) + 1) / 2, 2):
         print(digits[i])
 
 
