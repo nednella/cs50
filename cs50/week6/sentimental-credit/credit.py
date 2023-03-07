7a3183f44dc50f968a8600d2b1a89dcc09ec9bf5
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 22:24:29 +0000

    Tue, Mar 7, 2023, 10:24 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -7,13 +7,11 @@ def main():
     card = int(input("Card Number: "))              # prompt user for card number
     card = list(map(int, str(card)))                # convert credit card digits to an indexable list
 
-    reverse = card.reverse()
-
     if valid_Length(card) == False:                 # check if card length valid
         print("INVALID")
         sys.exit(1)
 
-    if luhn_checkSum(reverse) == False:                # check if card number valid as per Luhn's algorithm
+    if luhn_checkSum(reverse) == False:             # check if card number valid as per Luhn's algorithm
         print("INVALID")
         sys.exit(1)
 
@@ -21,7 +19,7 @@ def main():
 def test_func(reverse):
 
     digits = reverse
-    
+
     return
 
 
@@ -29,18 +27,17 @@ def test_func(reverse):
 
 
 def valid_Length(card):
-    if (13 <= len(card) <= 16):                      # check for valid card length (13 to 16 digits)
+    if (13 <= len(card) <= 16):                     # check for valid card length (13 to 16 digits)
         return True
     else:
         return False
 
 
 def luhn_checkSum(card):
-    #digits = card.reverse()                                  # reverse the indexable list of credit card digits
-    reverse = card.reverse()
+    reverse = card[::-1]                            # reverse the indexable list of credit card digits
 
-    even_Digits = reverse[0::2]                        # obtain list of every 2nd digit starting from the 0th digit
-    odd_Digits = reverse[1::2]                         # obtain list of every 2nd digit starting from the 1st digit
+    even_Digits = reverse[0::2]                     # obtain list of every 2nd digit starting from the 0th digit
+    odd_Digits = reverse[1::2]                      # obtain list of every 2nd digit starting from the 1st digit
 
     luhn_Sum = 0                                    # initialise algorithm sum
     luhn_Sum += sum(even_Digits)
