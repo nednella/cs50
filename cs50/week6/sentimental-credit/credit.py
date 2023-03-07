880f3943065661cf520b0ec04045b1607be37030
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 21:28:28 +0000

    Tue, Mar 7, 2023, 9:28 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -49,7 +49,7 @@ def luhn_Algorithm(card):
     #luhn_Sum += sum(even_Digits)
     #print(luhn_Sum)
 
-    luhn_Sum += sum(sum(divmod(d * 2, 10)) for d in digits)
+    luhn_Sum += sum(sum(divmod(d * 2, 10)) for d in odd_Digits)
     print(luhn_Sum)
 
     #for i in range(0, (len(digits) + 1) / 2, 2):
