c5cbec54b2a8a058aa050266a76ac623ba64ac39
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 17:17:42 +0000

    Tue, Mar 7, 2023, 5:17 PM +00:00

--- cs50/week6/world-cup/tournament.py
+++ cs50/week6/world-cup/tournament.py
@@ -31,6 +31,10 @@ def main():
     counts = {}
 
 
+
+
+
+
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
