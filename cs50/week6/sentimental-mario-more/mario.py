52b70e4927ddf1c8f45dab498a95c99fd33adfd3
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 18:02:16 +0000

    Tue, Mar 7, 2023, 6:02 PM +00:00

--- cs50/week6/sentimental-mario-more/mario.py
+++ cs50/week6/sentimental-mario-more/mario.py
@@ -5,9 +5,29 @@ while True:
     if height > 0 and height < 9:
         break
 
+# initialise counter
+i = 0
+j = i + 1
+k = 0
+l = 0
+
 for i in range(height):
 
-    for i + 1 in range(height):
-        print(" ", end="")
+    # print left middle-aligned triangle
+    for j in range(height):
+        print(" ", end = "")
+
+    for k in range(k <= i):
+        print("#", end = "")
+
+    # print space between the two triangles
+    print(" ", end = "")
+
+    # print right middle-aligned triangle
+    for l in range(l <= i):
+        print("#", end = "")
+
+    # return prompt to new line after each row is completed
+    print()
 
 
