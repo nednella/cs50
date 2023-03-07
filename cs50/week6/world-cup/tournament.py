14a2da4af7d23edcd1ed814b0cdf4388ed51fcd0
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 17:26:49 +0000

    Tue, Mar 7, 2023, 5:26 PM +00:00

--- cs50/week6/world-cup/tournament.py
+++ cs50/week6/world-cup/tournament.py
@@ -30,10 +30,13 @@ def main():
     # Simulate N tournaments and keep track of win counts
     counts = {}
 
+    for i in range(N):
+        winner = simulate_tournaments(teams)
 
-
-
-
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
