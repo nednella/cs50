865e253ef32347b7c76b61495ee55f9954b86a7b
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 18:55:31 +0000

    Tue, Mar 7, 2023, 6:55 PM +00:00

--- cs50/week6/sentimental-cash/cash.py
+++ cs50/week6/sentimental-cash/cash.py
@@ -1,11 +1,26 @@
 
 
 def main():
+    # ask what the customer is owed in change
+    change = get_payment()
 
+    # calculate number of quarters to give the customer
+    quarters = calc_quarters()
+
+    # calculate number of dimes to give the customer
+    dimes = calc_dimes()
+
+    # calculate number of nickels to give the customer
+    nickels = calc_dimes()
+
+    # calculate number of pennies to give the customer
+    pennies = calc_dimes()
+
+    # sum coins in the change
+    coins = quarters + dimes + nickels + pennies
 
 
 def get_payment():
-
     while True:
         try:
             change = float(input("Change owed: "))
@@ -14,8 +29,6 @@ def get_payment():
         else:
             if change >= 0:
                 return change
-
-
 
 
 def calc_quarters():
