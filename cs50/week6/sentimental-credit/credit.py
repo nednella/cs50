5634230542e36f30b777c5d88b2f9af7556baf14
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 20:00:36 +0000

    Tue, Mar 7, 2023, 8:00 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -1,16 +1,22 @@
 import sys
+import math
 
 
-def main()
+def main():
 
     # prompt user for input
-    card = input("Card Number: ")           # user input in string form
+    card = int(input("Card Number: "))           # user input in string form
 
-    if valid_length(card) == False:         # validate length in string form
-        print("INVALID")
-        sys.exit(1)
+    digits = (math.log10(card)) + 1
+    print(digits)
 
-    cc = int(card)                          # convert to integer
+
+
+    #if valid_length(card) == False:         # validate length in string form
+        #print("INVALID")
+        #sys.exit(1)
+
+    #cc = int(card)                          # convert to integer
 
 
 
@@ -23,14 +29,15 @@ def main()
 
 
 # check for valid card length (13 to 16 digits)
-def valid_length(card)
-    if (len(card) < 13 or len(card) > 16):
+def valid_length(card):
+    length = math.log10(card) + 1
+    if (length < 13 or length > 16):
         return False
     else:
         return True
 
 
-def get_digit(card, pos)
+def get_digit(card, pos):
     return card // 10**n % 10
 
 
