442e8c0d8a371d4cc67a0d78c3cf4e41935a7cf8
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 18:45:43 +0000

    Tue, Mar 7, 2023, 6:45 PM +00:00

---
+++ cs50/week6/sentimental-cash/cash.py
@@ -0,0 +1,36 @@
+
+
+def main():
+
+
+
+def get_cents():
+
+    while True:
+        try:
+            cents = int(input("Change owed: "))
+        except ValueError:
+            continue
+        else:
+            return cents
+
+
+
+
+def calc_quarters():
+
+
+
+def calc_dimes():
+
+
+
+def calc_nickels():
+
+
+
+def calc_pennies():
+
+
+
+main()
