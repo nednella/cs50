395128bc6a9b62ebe780a02e7834165d79874b2f
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-07 19:15:36 +0000

    Tue, Mar 7, 2023, 7:15 PM +00:00

--- cs50/week6/sentimental-cash/cash.py
+++ cs50/week6/sentimental-cash/cash.py
@@ -7,19 +7,23 @@ def main():
     quarters = calc_quarters(change)        # calculate quarters
     print(f"Quarters: {quarters}")
     change = change - (quarters * 0.25)     # update remaining change
-    print()
+    print(f"Change: {change}")
 
     dimes = calc_dimes(change)              # calculate dimes
     print(f"Dimes: {dimes}")
     change = change - (dimes * 0.1)         # update remaining change
+    print(f"Change: {change}")
+
 
     nickels = calc_nickels(change)          # calculate nickels
     print(f"Nickels: {nickels}")
     change = change - (nickels * 0.05)      # update remaining change
+    print(f"Change: {change}")
 
     pennies = calc_pennies(change)          # calculate pennies
     print(f"Pennies: {pennies}")
     change = change - (pennies * 0.01)      # update remaining change
+    print(f"Change: {change}")
 
     # calculate total coin quantity and print
     coins = quarters + dimes + nickels + pennies
