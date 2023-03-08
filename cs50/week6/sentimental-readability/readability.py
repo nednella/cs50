f7fbea0a7e9c141e8da9f4b15c6eca3b4997ce6f
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 16:44:32 +0000

    Wed, Mar 8, 2023, 4:44 PM +00:00

--- cs50/week6/sentimental-readability/readability.py
+++ cs50/week6/sentimental-readability/readability.py
@@ -14,13 +14,11 @@
 
 
 def main():
-    text = input("Text: ")
-
+    text = user_input()
 
     letters = count_letters(text)
     words = count_words(text)
     sentences = count_sentences(text)
-
 
     print(f"Letters: {letters}")
     print(f"Words: {words}")
@@ -34,18 +32,15 @@ def main():
     #print(f"Numbers: {numbers}")
 
 
-#def user_input():
+def user_input():                           # obtain input and return if input is non-blank
     while True:
         try:
             text = input("Text: ")
-        except
-
-    text = input("Text: ")
-
-
-
-
-
+        except ValueError:
+            continue
+        else:
+            if len(text) > 0:
+                return text
 
 
 def count_letters(text):                    # ASSUMPTION: any alphabetical character a-z or A-Z is a letter
@@ -66,7 +61,19 @@ def count_sentences(text):                  # ASSUMPTION: any occurrence of . ! 
     return sentences
 
 
-def grade(text):
+def grade(letters, words, sentences):
+    L = float(letters / words) * 100
+    S = float(sentences / words) * 100
+    index = float(0.0588 * L - 0.296 * S - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        grade = int(round(index))       # round float, then cast to an int
+        print(f"Grade {grade}")
+
     grade = 0
     return grade
 
