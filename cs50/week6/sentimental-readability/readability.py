3cd99c4f4460ccf5dbdb54e69096df6e004fcefa
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 16:55:00 +0000

    Wed, Mar 8, 2023, 4:55 PM +00:00

--- cs50/week6/sentimental-readability/readability.py
+++ cs50/week6/sentimental-readability/readability.py
@@ -1,18 +1,4 @@
 
-# program outline
-
-# prompt user for an entry of text
-# output the grade level for the text - following Coleman-Liau index
-    # count the number of letters
-        # can assume a letter is lowercase A to Z, or uppercase A to Z
-    # count the number of words
-        # any sequence of characters separated by a space dictates the end of a word
-    # count the number of sentences
-        # can assume any occurence of . ! ? dictates the end of a sentence
-
-
-
-
 def main():
     # obtain user input
     text = user_input()
@@ -22,24 +8,20 @@ def main():
     words = count_words(text)
     sentences = count_sentences(text)
 
-    # determine grade via Coleman-Liau index
+    # determine grade of string via Coleman-Liau index
     index = cl_index(letters, words, sentences)
 
-    #print(f"Letters: {letters}")
-    #print(f"Words: {words}")
-    #print(f"Sentences: {sentences}")
-
-    # output text grade
+    # output grade of string
     if index < 1:
         print("Before Grade 1")
     elif index > 16:
         print("Grade 16+")
     else:
-        index = int(round(index))               # round float, then cast to an int
+        index = int(round(index))                   # round float, then cast to an int
         print(f"Grade {index}")
 
 
-def user_input():                               # obtain input and return if input is non-blank
+def user_input():                                   # obtain input and return if input is non-blank
     while True:
         try:
             text = input("Text: ")
@@ -50,17 +32,17 @@ def user_input():                               # obtain input and return if inp
                 return text
 
 
-def count_letters(text):                        # ASSUMPTION: any alphabetical character a-z or A-Z is a letter
+def count_letters(text):                            # ASSUMPTION: any alphabetical character a-z or A-Z is a letter
     letters = sum(c.isalpha() for c in text)
     return letters
 
 
-def count_words(text):                          # ASSUMPTION: any sequence of characters separated by a space is a word
-    words = len(text.split())                   # split string into a list of words, count length of list
+def count_words(text):                              # ASSUMPTION: any sequence of characters separated by a space is a word
+    words = len(text.split())                           # split string into a list of words, count length of list
     return words
 
 
-def count_sentences(text):                      # ASSUMPTION: any occurrence of . ! ? indicates the end of a sentence
+def count_sentences(text):                          # ASSUMPTION: any occurrence of . ! ? indicates the end of a sentence
     sentences = 0
     sentences += text.count('.')
     sentences += text.count('?')
@@ -68,10 +50,10 @@ def count_sentences(text):                      # ASSUMPTION: any occurrence of 
     return sentences
 
 
-def cl_index(letters, words, sentences):        # calculate Coleman-Liau index
-    L = float(letters / words) * 100
-    S = float(sentences / words) * 100
-    index = float(0.0588 * L - 0.296 * S - 15.8)
+def cl_index(letters, words, sentences):            # calculate Coleman-Liau index
+    L = float(letters / words) * 100                    # L - average no. of letters per 100 words
+    S = float(sentences / words) * 100                  # S - average no. of sentences per 100 words
+    index = float(0.0588 * L - 0.296 * S - 15.8)        # Coleman-Liau index formula
     return index
 
 
