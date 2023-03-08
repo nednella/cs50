489ab33fb7b8dfddc067a1875336e4be2685cee3
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 23:18:29 +0000

    Wed, Mar 8, 2023, 11:18 PM +00:00

--- cs50/week6/sentimental-mario-less/mario.py
+++ cs50/week6/sentimental-mario-less/mario.py
@@ -1,1 +1,13 @@
-# TODO
+
+while True:
+
+    try:
+        height = int(input("Height: "))
+    except ValueError:
+        continue
+    else:
+        if height < 9 and height > 0:
+            break
+
+for i in range(height):
+    
