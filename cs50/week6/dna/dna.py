94d64d023db7116df1e8fad3e4dfb1c7a094b98c
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 20:53:43 +0000

    Wed, Mar 8, 2023, 8:53 PM +00:00

--- cs50/week6/dna/dna.py
+++ cs50/week6/dna/dna.py
@@ -11,8 +11,8 @@ def main():
 
 
     # TODO: Read database file into a variable
-    with open("/databases/small.csv", "r") as file:                           #sys.argv[1]
-        reader = csv.dictReader(file)
+    with open("databases/small.csv", "r") as file:                           #sys.argv[1]
+        reader = csv.DictReader(file)
 
 
     # TODO: Read DNA sequence file into a variable
