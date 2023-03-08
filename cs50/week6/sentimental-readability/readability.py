aea49586353c46548e16aeba81c359c89fb61e01
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 15:43:48 +0000

    Wed, Mar 8, 2023, 3:43 PM +00:00

--- cs50/week6/sentimental-readability/readability.py
+++ cs50/week6/sentimental-readability/readability.py
@@ -1,4 +1,15 @@
 
+# program outline
+
+# prompt user for an entry of text
+# output the grade level for the text - following Coleman-Liau index
+    # count the number of letters
+    # count the number of words
+    # 
+
+
+
+
 def main():
 
 
