1a80cabdf5e034b77680ce8e52bd67b16bb1882a
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-08 01:24:05 +0000

    Wed, Mar 8, 2023, 1:24 AM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -1,50 +1,55 @@
 import sys
-import math
 
 
 def main():
 
-    card = int(input("Card Number: "))                      # prompt user for card number
-    card = list(map(int, str(card)))                        # convert credit card digits to an indexable list
+    card = int(input("Card Number: "))              # prompt user for card number
+    card = list(map(int, str(card)))                # convert credit card digits to an indexable list
 
-    #if valid_Length(card) == False:                 # check if card length valid
-        #print("INVALID")
-        #sys.exit(1)
 
-    if luhn_checkSum(card) == False:                        # check if card number valid as per Luhn's algorithm
+    if valid_Length(card) == False or luhn_checkSum(card) == False:     # check if card valid
+        print("INVALID")
+        sys.exit(1)
+
+    if amex(card) == True:
+        print("AMEX")
+        sys.exit(0)
+
+    elif visa(card) == True:
+        print("AMEX")
+        sys.exit(0)
+
+    elif mastercard(card) == True:
+        print("AMEX")
+        sys.exit(0)
+    else:
         print("INVALID")
         sys.exit(1)
 
 
-
-
-
-
-
 def valid_Length(card):
-    if (13 <= len(card) <= 16):                             # check for valid card length (13 to 16 digits)
+    if (13 <= len(card) <= 16):                     # check for valid card length (13 to 16 digits)
         return True
     else:
         return False
 
 
 def luhn_checkSum(card):
-    reverse = card[::-1]                                    # reverse the indexable list of credit card digits
+    reverse = card[::-1]                            # reverse the indexable list of credit card digits
 
-    even_Digits = reverse[0::2]                             # obtain list of every 2nd digit starting from the 0th digit
-    odd_Digits = reverse[1::2]                              # obtain list of every 2nd digit starting from the 1st digit
+    even_Digits = reverse[0::2]                     # obtain list of every 2nd digit starting from the 0th digit
+    odd_Digits = reverse[1::2]                      # obtain list of every 2nd digit starting from the 1st digit
 
-    luhn_Sum = 0                                            # initialise algorithm sum
+    luhn_Sum = 0                                    # initialise algorithm sum
     luhn_Sum += sum(even_Digits)
 
     for d in odd_Digits:
         luhn_Sum += sum(divmod(d * 2, 10))
 
-    if luhn_Sum % 10 == 0:                                  # check if luhn sum is a factor of 10
+    if luhn_Sum % 10 == 0:                          # check if luhn sum is a factor of 10
         return True
     else:
         return False
-
 
 
 def amex(card):
@@ -64,8 +69,7 @@ def visa(card):
 
 def mastercard(card)
     nums = int(str(card[0]) + str(card[1]))                 # concatenate first two digits of the card
-    if len(card) == 16 and (nums == 51 or nums == 52 or nums == 53 or nums == 54 or nums == 55)
-
+    if len(card) == 16 and (nums == 51 or nums == 52 or nums == 53 or nums == 54 or nums == 55)     # check for Mastercard properties
 
 
 main()
