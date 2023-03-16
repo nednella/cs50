52663bc20e292ccaaf0a1c01337c9971122ad82c
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 18:51:51 +0000

    Thu, Mar 16, 2023, 6:51 PM +00:00

--- cs50/week7/movies/9.sql
+++ cs50/week7/movies/9.sql
@@ -3,19 +3,6 @@ FROM people
 WHERE id IN (
     SELECT person_id
     FROM stars
-    WHERE movie_id = (
-        SELECT id
-        FROM movies
-        WHERE title = 'Toy Story'
-    )
-);
-
-
-SELECT name, birth
-FROM people
-WHERE id IN (
-    SELECT person_id
-    FROM stars
     WHERE movie_id IN (
         SELECT id
         FROM movies
