00dc1fa6d9177a24909897df1827450a92ec724b
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 20:22:30 +0000

    Thu, Mar 16, 2023, 8:22 PM +00:00

--- cs50/week7/movies/13.sql
+++ cs50/week7/movies/13.sql
@@ -1,48 +1,23 @@
-SELECT title
-FROM movies
-WHERE id IN (
-    SELECT movie_id
-    FROM stars
-    WHERE person_id = (
-        SELECT id
-        FROM people
-        WHERE name = 'Johnny Depp'
-    )
-) AND id IN (
-    SELECT movie_id
-    FROM stars
-    WHERE person_id = (
-        SELECT id
-        FROM people
-        WHERE name = 'Helena Bonham Carter'
-    )
-);
-
-
-
-
-
 SELECT name
 FROM people
 WHERE id IN (
-
-
-
-) AND name != 'Kevin Bacon'
-
-
-SELECT person_id
-FROM stars
-WHERE movie_id IN (
-    SELECT id
-    FROM movies
-    WHERE id IN (
-        SELECT movie_id
-        FROM stars
-        WHERE person_id = (
-            SELECT id
-            FROM people
-            WHERE name = 'Kevin Bacon' AND birth = '1958'
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN (
+        SELECT id
+        FROM movies
+        WHERE id IN (
+            SELECT movie_id
+            FROM stars
+            WHERE person_id = (
+                SELECT id
+                FROM people
+                WHERE name = 'Kevin Bacon' AND birth = '1958'
+            )
         )
     )
 )
+AND name != 'Kevin Bacon';
+
+
+
