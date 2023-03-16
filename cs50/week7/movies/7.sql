55e07ec475367f245e60f240114d6c7b98326472
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 18:23:42 +0000

    Thu, Mar 16, 2023, 6:23 PM +00:00

--- cs50/week7/movies/7.sql
+++ cs50/week7/movies/7.sql
@@ -1,11 +1,4 @@
 SELECT movies.title, ratings.rating
-FROM movies WHERE year = '2010'
-JOIN ratings
-ON movies.id = ratings.movie_id LIMIT 10;
---ORDER BY ratings.rating DESC;
-
-
-SELECT movies.title, ratings.rating
 FROM movies
 JOIN ratings
 ON movies.id = ratings.movie_id
