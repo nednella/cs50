8bad704ce142826ae61dd9aaf7f53cf58ee35a23
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 19:21:51 +0000

    Thu, Mar 16, 2023, 7:21 PM +00:00

--- cs50/week7/movies/12.sql
+++ cs50/week7/movies/12.sql
@@ -2,17 +2,19 @@ SELECT title
 FROM movies
 WHERE id IN (
 
-)
+
 
 SELECT movie_id
 FROM stars
 WHERE person_id = (
     SELECT id
     FROM people
-    WHERE name = 'Johnny Depp'
+    WHERE name = 'Helena Bonham Carter'
 )
+)
+;
 AND person_id = (
     SELECT id
     FROM people
     WHERE name = 'Helena Bonham Carter'
-)
+)'Johnny Depp'
