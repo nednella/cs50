bba6be333e9ec5c1230fd4b0292479e90be70f2b
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 19:05:16 +0000

    Thu, Mar 16, 2023, 7:05 PM +00:00

--- cs50/week7/movies/11.sql
+++ cs50/week7/movies/11.sql
@@ -1,4 +1,4 @@
-SELECT movies.title, ratings.rating
+SELECT movies.title
 FROM movies
 JOIN ratings
 ON movies.id = ratings.movie_id
---
+++ cs50/week7/movies/12.sql
@@ -0,0 +1,10 @@
+SELECT title
+FROM movies
+WHERE id IN (
+
+)
+
+
+SELECT id, name
+FROM people
+WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter';
