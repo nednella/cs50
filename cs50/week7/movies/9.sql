f1703cf5bc77e540dd2025cb2d7554cd88561b2e
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-16 18:51:07 +0000

    Thu, Mar 16, 2023, 6:51 PM +00:00

--- cs50/week7/movies/9.sql
+++ cs50/week7/movies/9.sql
@@ -11,18 +11,19 @@ WHERE id IN (
 );
 
 
-SELECT name
+SELECT name, birth
 FROM people
 WHERE id IN (
-
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN (
+        SELECT id
+        FROM movies
+        WHERE year = '2004'
+    )
 )
+ORDER BY birth ASC;
 
-SELECT person_id
-FROM stars
-WHERE movie_id IN (
-    SELECT id,
-    FROM movies
-    WHERE year = '2004')
-LIMIT 10;
+
 
 
