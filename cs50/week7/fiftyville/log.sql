1eef8ba679bc794a009231b1a49f7c5dc343c4f0
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 19:31:16 +0000

    Mon, Mar 20, 2023, 7:31 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -3,11 +3,7 @@
 -- understand the contents and layout of the stored data
 .schema
 
--- check what information is stored in the crime scene reports
-SELECT *
-FROM crime_scene_reports;
-
--- check the crime scene reports on the day that the theft of the CS50 duck took place
+-- check what information is stored in the crime scene reports on the day that the theft of the CS50 duck took place
 SELECT *
 FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28;
@@ -15,7 +11,9 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 -- case ID 295 - CS50 duck theft.
 -- 10:15am at the Humprhrey Street bakery. 3 witnesses, all were interviewed.
 
---
+-- check what information is stored in the interviews database on the day of the theft
 SELECT *
 FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28;
+
+-- 
