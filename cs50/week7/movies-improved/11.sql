65ff457a773e8ac23353cc676f2865681535174a
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 16:30:09 +0000

    Mon, Mar 20, 2023, 4:30 PM +00:00

--- cs50/week7/movies-improved/11.sql
+++ cs50/week7/movies-improved/11.sql
@@ -4,4 +4,5 @@ JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = 'Chadwick Boseman'
-ORDER BY ratings.rating DESC;
+ORDER BY ratings.rating DESC
+LIMIT 5;
