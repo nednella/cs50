d2c582043b6bf19b15cab79442111c0cd2ea093b
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 19:48:54 +0000

    Mon, Mar 20, 2023, 7:48 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -18,7 +18,7 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 
 -- witnesses to the CS50 duck theft are:
 -- Ruth (Interview ID 161)     - Within 10 minutes of the theft, she saw thief get into a car in the bakery parking lot.
--- Eugene (Interview ID 162)   - He recognised the thief, but doesn't know their name. He saw the thief withdrawing money from the ATM on the same morning
+-- Eugene (Interview ID 162)   - He recognised the thief, but doesn't know their name. He saw the thief withdrawing money from the ATM on Leggett Street on the same morning.
 -- Raymond (Interview ID 163)  - Saw the thief call someone and talk for less than 1 minute. Heard the thief state they would take the earliest flight out of Fiftyville tomorrow (29th July 2021), and asked them to buy a plane ticket.
 
 -- check security camera footage for the date and time of the theft
@@ -39,7 +39,35 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
 -- check ATM records for the date of the theft
 SELECT *
 FROM atm_transactions
-WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Humphrey Lane' AND transaction_type = 'withdraw';
+WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
+
+-- there are 8 total withdrawal transactions that match the information in Eugene's interview
+-- ID 246, account number 28500762, amount 48
+-- ID 264, account number 28296815, amount 20
+-- ID 266, account number 76054385, amount 60
+-- ID 267, account number 49610011, amount 50
+-- ID 269, account number 16153065, amount 80
+-- ID 288, account number 25506511, amount 20
+-- ID 313, account number 81061156, amount 30
+-- ID 336, account number 26013199, amount 35
+
+-- check phone calls for the date of the theft, with less than 1 minute duration
+SELECT *
+FROM phone_calls
+WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
+
+-- there are 9 total phone calls that took place on the day of the theft with less than 1 minute duration
+-- ...
+
+-- 
+
+
+
+
+
+
+
+
 
 
 
