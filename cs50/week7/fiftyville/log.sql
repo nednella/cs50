61a11310255c89e0ad3829cadd64878be768aba6
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 20:40:42 +0000

    Mon, Mar 20, 2023, 8:40 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -90,6 +90,21 @@ WHERE people.license_plate IN (
 );
 
 
+SELECT *
+FROM people
+WHERE license_plate IN (
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25;
+) AND account_number IN (
+    SELECT account_number
+    FROM atm_transactions
+    WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
+) AND phone_number IN (
+
+)
+
+
 
 
 
