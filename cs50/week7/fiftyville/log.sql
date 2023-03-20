45ede849cc50cecba91d8f33c1cbaa906ab7150e
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 19:28:38 +0000

    Mon, Mar 20, 2023, 7:28 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -1,2 +1,18 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- understand the contents and layout of the stored data
+.schema
+
+-- check what information is stored in the crime scene reports
+SELECT *
+FROM crime_scene_reports;
+
+-- check the crime scene reports on the day that the theft of the CS50 duck took place
+SELECT *
+FROM crime_scene_reports
+WHERE year = 2021 AND month = 7 AND day = 28;
+
+-- case ID 295 - CS50 duck theft.
+-- 10:15am at the Humprhrey Street bakery. 3 witnesses, all were interviewed.
+
+-- 
