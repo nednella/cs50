5b80d415aa0b62b84e66da0fff4c3e66ffa07aeb
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 20:27:08 +0000

    Mon, Mar 20, 2023, 8:27 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -90,6 +90,33 @@ WHERE bank_accounts.account_number IN (
 
 
 
+
+SELECT *
+FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN phone_calls ON phone_calls.caller = people.phone_number
+WHERE people.license_plate IN (
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25
+) AND people.account_number IN (
+    SELECT account_number
+    FROM atm_transactions
+    WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
+) AND people.phone_number IN (
+    SELECT caller
+    FROM phone_calls
+    WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60
+);
+
+
+
+
+
+
+
+
 -- check for records of people matching any of the 8 license plates and 9 outbound callers from the above queries
 SELECT *
 FROM people
@@ -105,7 +132,7 @@ WHERE license_plate IN (
 
 
 
--- check for any matches 
+-- check for any matches
 
 
 
