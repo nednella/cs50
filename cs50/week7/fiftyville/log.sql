6fc0a8b1839768348f6c2bb3c2c9ea0fa22025ce
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 20:11:34 +0000

    Mon, Mar 20, 2023, 8:11 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -72,7 +72,7 @@ WHERE license_plate IN (
     WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60
 );
 
--- there are 4 matches for people who's cars left the bakery within 10 minutes of the theft, and who's phone numbers had a call with less than 1 minute duration on the day of the theft:
+-- there are 4 matches for people who's cars left the bakery within 10 minutes of the theft, and who's phone numbers had an outbound call with less than 1 minute duration on the day of the theft:
 -- ID 398010, name Sofia, phone number (130) 555-0289, passport number 1695452385, license plate G412CB7
 -- ID 514354, name Diana, phone number (770) 555-1861, passport number 3592750733, license plate 322W7JE
 -- ID 560886, name Kelsey, phone number (499) 555-9472, passport number 8294398571, license plate 0NTHK55
@@ -93,18 +93,42 @@ WHERE caller IN (
         FROM phone_calls
         WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60
     )
-);
+) AND year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
+-- there are 5 matches for people who received a call from any of the 4 individuals in the query above
+-- ...
 
-
-
-
-
+-- check the identities of the 5 people in the previous query
 SELECT *
 FROM people
 WHERE phone_number IN (
+    SELECT receiver
+    FROM phone_calls
+    WHERE caller IN (
+        SELECT phone_number
+        FROM people
+        WHERE license_plate IN (
+            SELECT license_plate
+            FROM bakery_security_logs
+            WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25
+        ) AND phone_number IN (
+            SELECT caller
+            FROM phone_calls
+            WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60
+        )
+    ) AND year = 2021 AND month = 7 AND day = 28 AND duration < 60
+);
 
-)
+-- the identities of possible people who received a call from the culprit asking to buy a flight ticket for 29th July 2021 are:
+-- ID 251693, name Larry, phone number (892) 555-8872, passport number 2312901747, license plate O268ZZ0
+-- ID 567218, name jack, phone number (996) 555-8899, passport number 9029462229, license plate 52R0Y8U
+-- ID 626361, name Melissa, phone number (717) 555-1342, passport number 7834357192, license plate N/A
+-- ID 847116, name Philip, phone number (725) 555-3243, passport number 3391710505, license plate GW362R6,
+-- ID 864400, name Robin, phone number (375) 555-8161, passport number N/A, license plate 4V16VO0
+
+
+
+
 
 
 
