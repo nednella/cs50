2a361e4d0c8c29aae3cbd23139081214cbed9083
benallenden <119449811+benallenden@users.noreply.github.com>
2023-03-20 19:35:50 +0000

    Mon, Mar 20, 2023, 7:35 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -16,4 +16,10 @@ SELECT *
 FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28;
 
--- 
+-- witnesses to the CS50 duck theft are:
+-- Ruth (Interview ID 161)     - Within 10 minutes of the theft, she saw thief get into a car in the bakery parking lot.
+-- Eugene (Interview ID 162)   - He recognised the thief, but doesn't know their name. He saw the thief withdrawing money from the ATM on the same morning
+-- Raymond (Interview ID 163)  - Saw the thief call someone and talk for less than 1 minute. Heard the thief state they would take the earliest flight out of Fiftyville tomorrow (29th July 2021), and asked them to buy a plane ticket.
+
+-- check security camera footage
+
