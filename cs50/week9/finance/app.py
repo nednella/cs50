feb841db0a9698210c60488f7a7b6ed03493997d
benallenden <119449811+benallenden@users.noreply.github.com>
2023-04-11 20:26:58 +0000

    Tue, Apr 11, 2023, 9:26 PM +01:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -118,7 +118,7 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
-
+        
         if not username:
             return apology("must provide username", 403)
         elif not password:
