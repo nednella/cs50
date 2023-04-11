61cf82b15f082d6e0ed11aa7c3f71ef6a065c58d
benallenden <119449811+benallenden@users.noreply.github.com>
2023-04-11 15:50:28 +0000

    Tue, Apr 11, 2023, 4:50 PM +01:00

--- cs50/week9/birthdays/app.py
+++ cs50/week9/birthdays/app.py
@@ -12,7 +12,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
-
+# Global dictionary of birthdays
+BIRTHDAYS = {}
 
 
 @app.after_request
