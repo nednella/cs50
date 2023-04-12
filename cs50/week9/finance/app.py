aaf26c49a6fa9ff69d987920f095c07ef8d2e478
benallenden <119449811+benallenden@users.noreply.github.com>
2023-04-12 17:16:28 +0000

    Wed, Apr 12, 2023, 6:16 PM +01:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -109,7 +109,7 @@ def logout():
 def quote():
     """Get stock quote."""
     if request.method == "POST":
-        symbol = request.form.get("symbol")
+        symbol = request.form.get("symbol").upper
         if not symbol:
             return apology("Please enter a symbol!", 403)
 
